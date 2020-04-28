5033a34423d869bbcad185be17c95234440ddfb3
randallquesadaa <48957125+randallquesadaa@users.noreply.github.com>
2020-04-27 18:06:36 -0600

    prueba deploy automatico

--- footer.php
+++ footer.php
@@ -1,6 +1,7 @@
  <!-- footer -->
         <footer class="row bg-dark justify-content-center">
             <p class="text-white">Copyright &copy; 2020 Arabella</p>
+            <p>Prueba Deploy Automatico</p>
         </footer>
         <!-- footer -->
 
