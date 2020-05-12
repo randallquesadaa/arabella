5a8c5fffefc2a6aab00f5dc19412dc7b769b5e7e
randallquesadaa <48957125+randallquesadaa@users.noreply.github.com>
2020-05-12 00:20:00 -0600

    8 new products

--- includes/section-newproducts.php
+++ includes/section-newproducts.php
@@ -2,14 +2,14 @@
     $args = array(
     'post_type' => 'product',
     'stock' => 1,
-    'posts_per_page' => 6,
+    'posts_per_page' => 8,
     'orderby' =>'date',
     'order' => 'DESC' );
     $loop = new WP_Query( $args );
 ?>
     <div class="row">
         <?php while ( $loop->have_posts() ) : $loop->the_post(); global $product; ?>
-        <div class="col-lg-4 col-6 d-flex justify-content-lg-center my-lg-3 p-0">
+        <div class="col-lg-3 col-6 d-flex justify-content-lg-center my-lg-3 p-0">
         <a id="id-<?php the_id(); ?>" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
         <?php if (has_post_thumbnail( $loop->post->ID )) echo get_the_post_thumbnail($loop->post->ID, 'shop_catalog'); else echo '<img class="img-fluid" src="'.woocommerce_placeholder_img_src().'" alt="My Image Placeholder" width="65px" height="115px" />'; ?>
         </a>
