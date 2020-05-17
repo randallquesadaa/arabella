1f503ba989bb216ea55f8b1dc58e6e2b12364a94
randallquesadaa <48957125+randallquesadaa@users.noreply.github.com>
2020-05-17 11:10:43 -0600

    update section-categories

--- includes/section-categories.php
+++ includes/section-categories.php
@@ -1,5 +1,4 @@
 <?php
-
   $taxonomy     = 'product_cat';
   $orderby      = 'name';  
   $show_count   = 0;      // 1 for yes, 0 for no
@@ -21,7 +20,41 @@
  foreach ($all_categories as $cat) {
     if($cat->category_parent == 0) {
         $category_id = $cat->term_id;       
-        echo '<br /><a id="'.$cat->term_id.'" onClick="'.$cat->name.'" href="'. get_term_link($cat->slug, 'product_cat') .'">'. $cat->name .'</a>';
+        echo '<br /><a id="'.$cat->term_id.'" href="'. get_term_link($cat->slug, 'product_cat') .'">'. $cat->name .'</a>';
     }       
 }
+
+$host= $_SERVER["HTTP_HOST"];
+$link= $_SERVER["REQUEST_URI"];
+$url="http://" . $host . $link;
+echo $url;
+
+/*if($url == "http://localhost/arabella/categoria-producto/accesorios/")
+    {
+        $taxonomy     = 'product_cat';
+    $orderby      = 'name';  
+    $show_count   = 0;      // 1 for yes, 0 for no
+    $pad_counts   = 0;      // 1 for yes, 0 for no
+    $hierarchical = 1;      // 1 for yes, 0 for no  
+    $title        = '';  
+    $empty        = 0;
+      
+    $args = array(
+            'taxonomy'     => $taxonomy,
+            'child_of'     => 0,
+            'parent'       => 26,
+            'orderby'      => $orderby,
+            'show_count'   => $show_count,
+            'pad_counts'   => $pad_counts,
+            'hierarchical' => $hierarchical,
+            'title_li'     => $title,
+            'hide_empty'   => $empty
+    );
+    $sub_cats = get_categories( $args );
+    if($sub_cats) {
+        foreach($sub_cats as $sub_category) {
+            echo '<br /><a id="'.$sub_category->term_id.'" onClick="'.$sub_category->name.'" href="'. get_term_link($sub_category->slug, 'product_cat') .'">'. $sub_category->name .'</a>';
+        }   
+    }
+    }*/
 ?>
