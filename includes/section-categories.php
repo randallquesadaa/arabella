4228efeaf5cf9f9c6840f7b0143ef6e95bbd8b39
Jean Alpizar <jstevenla1828@gmail,com>
2020-05-17 18:25:57 -0600

    se hace el backend de las categorias/tienda

--- includes/section-categories.php
+++ includes/section-categories.php
@@ -6,55 +6,68 @@
   $hierarchical = 1;      // 1 for yes, 0 for no  
   $title        = '';  
   $empty        = 0;
+  $array_id = array();
+  $array_name = array();
+  $longitud     = 0;
+  $link= $_SERVER["REQUEST_URI"]; 
 
   $args = array(
-         'taxonomy'     => $taxonomy,
-         'orderby'      => $orderby,
-         'show_count'   => $show_count,
-         'pad_counts'   => $pad_counts,
-         'hierarchical' => $hierarchical,
-         'title_li'     => $title,
-         'hide_empty'   => $empty
-  );
- $all_categories = get_categories( $args );
- foreach ($all_categories as $cat) {
-    if($cat->category_parent == 0) {
-        $category_id = $cat->term_id;       
-        echo '<br /><a id="'.$cat->term_id.'" href="'. get_term_link($cat->slug, 'product_cat') .'">'. $cat->name .'</a>';
-    }       
-}
-
-$host= $_SERVER["HTTP_HOST"];
-$link= $_SERVER["REQUEST_URI"];
-$url="http://" . $host . $link;
-echo $url;
-
-/*if($url == "http://localhost/arabella/categoria-producto/accesorios/")
-    {
-        $taxonomy     = 'product_cat';
-    $orderby      = 'name';  
-    $show_count   = 0;      // 1 for yes, 0 for no
-    $pad_counts   = 0;      // 1 for yes, 0 for no
-    $hierarchical = 1;      // 1 for yes, 0 for no  
-    $title        = '';  
-    $empty        = 0;
-      
-    $args = array(
+    'taxonomy'     => $taxonomy,
+    'orderby'      => $orderby,
+    'show_count'   => $show_count,
+    'pad_counts'   => $pad_counts,
+    'hierarchical' => $hierarchical,
+    'title_li'     => $title,
+    'hide_empty'   => $empty
+);
+  if($link == '/wordpress/shop/'){
+    
+    $all_categories = get_categories( $args );
+    
+    foreach ($all_categories as $cat) {
+        if($cat->category_parent == 0) {
+            $category_id = $cat->term_id;       
+            echo '<br /><a id="'.$cat->term_id.'" href="'. get_term_link($cat->slug, 'product_cat') .'">'. $cat->name .'</a>';
+            array_push($array_name, $cat->slug);
+            array_push($array_id, $cat->term_id);
+            
+          }       
+        
+        }
+    }
+        //Si la longitud es cero que cuente las sub categorias
+        if($longitud == 0){
+            $all_categories = get_categories( $args );
+            foreach ($all_categories as $cat) {
+                if($cat->category_parent == 0) {
+                    $category_id = $cat->term_id;       
+                    array_push($array_name, $cat->slug);
+                    array_push($array_id, $cat->term_id);
+                }
+            }
+        } 
+        $longitud = count($array_id);
+        for($i=0; $i<$longitud; $i++){
+        if($link == '/wordpress/product-category/'.$array_name[$i].'/'){
+            $args2 = array(
             'taxonomy'     => $taxonomy,
             'child_of'     => 0,
-            'parent'       => 26,
+            'parent'       => $array_id[$i],
             'orderby'      => $orderby,
             'show_count'   => $show_count,
             'pad_counts'   => $pad_counts,
             'hierarchical' => $hierarchical,
             'title_li'     => $title,
             'hide_empty'   => $empty
-    );
-    $sub_cats = get_categories( $args );
-    if($sub_cats) {
-        foreach($sub_cats as $sub_category) {
-            echo '<br /><a id="'.$sub_category->term_id.'" onClick="'.$sub_category->name.'" href="'. get_term_link($sub_category->slug, 'product_cat') .'">'. $sub_category->name .'</a>';
-        }   
+            );
+            $sub_cats = get_categories( $args2 );
+            if($sub_cats) {
+                foreach($sub_cats as $sub_category) {
+                    echo '<br /><a id="'.$sub_category->term_id.'" href="'. get_term_link($sub_category->slug, 'product_cat') .'">'. $sub_category->name .'</a>';
+                }   
+            }
+        }
     }
-    }*/
+
+    
 ?>
