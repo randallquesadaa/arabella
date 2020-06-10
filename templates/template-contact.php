c075969b8576ac6b804fc5ffb96dc5692e1a4511
luismi97 <luismiguelmolina8@gmail.com>
2020-06-09 22:39:15 -0600

    p
    p

--- templates/template-contact.php
+++ templates/template-contact.php
@@ -29,47 +29,6 @@ Template Name: Contact
     </div>
 </section>
 
-<section class="container-fluid contact">
-    <div class="row d-flex justify-content-center">
-        <div class="col-lg-10 col-12 my-5">
-            <h2>Get In Touch</h2>
 
-            <form>
-                <div class="row">
-                    <div class="col-lg-6 my-2">
-                        <input type="text" class="form-control" placeholder="First name">
-                    </div>
-                    <div class="col-lg-6 my-2">
-                        <input type="text" class="form-control" placeholder="Last name">
-                    </div>
-                    <div class="col-lg-12 my-2">
-                        <input type="text" class="form-control" placeholder="Email">
-                    </div>
-                    <div class="col-lg-12 my-2">
-                        <input type="text" class="form-control" placeholder="Subject">
-                    </div>
-                    <div class="col-lg-12 my-2">
-                        <input type="text" class="form-control " placeholder="Message">
-                    </div>
-                </div>
-                <button class="btn btn-dark bt-2 float-lg-right">Submit</button>
-            </form>
-
-            <div class="row mt-4 text-center text-lg-left">
-                    <div class="col-lg-6 col-6 p-0 d-flex justify-content-center">
-                        <a class="d-flex" href="">
-                            <i class="fas fa-envelope"></i>
-                            <p class="mb-0 ml-2">arabella@gmail.com</p>
-                        </a>
-                    </div>
-                    <div class="col-lg-6 col-6 p-0 d-flex justify-content-center">
-                        <a class="d-flex" href="">
-                            <i class="fas fa-phone-alt"></i>
-                            <p class="mb-0 ml-2">+506 2222 2222</p>
-                        </a>
-                    </div>
-            </div>
-    </div>
-</section>
 
 <?php get_footer();?>
