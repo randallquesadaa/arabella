1184471bd70ba9b2126c9ace991cf792e438641a
randallquesadaa <48957125+randallquesadaa@users.noreply.github.com>
2020-06-29 23:36:37 -0600

    update personalizacion

--- templates/template-contact.php
+++ templates/template-contact.php
@@ -9,11 +9,13 @@ Template Name: Contact
 <section>
     <div>
         <div class="hero" id="site-header">
-            <img src="<?php echo wp_get_attachment_url(get_theme_mod('lwp-home-callout-imgContact'))?>">
+            <img src="<?php echo wp_get_attachment_url(get_theme_mod('lwp-img-callout-imgContact'))?>">
         </div>
         <div class="hero-contact container">
             <div class="row">
-                <h3 class="text-white text-center text-lg-left">About us and how to contact</h3>
+                <h3 class="text-white text-center text-lg-left">
+                    <?php echo get_theme_mod('lwp-en-contact-callout-title')?>
+                </h3>
             </div>
         </div>
     </div>
@@ -28,15 +30,19 @@ Template Name: Contact
 
             <div class="row mt-4 text-center text-lg-left">
                     <div class="col-lg-6 col-6 p-0 d-flex justify-content-center">
-                        <a class="d-flex" href="">
+                        <a class="d-flex" href="<?php echo get_theme_mod('lwp-en-home-callout-email')?>">
                             <i class="fas fa-envelope"></i>
-                            <p class="mb-0 ml-2">arabella@gmail.com</p>
+                            <p class="mb-0 ml-2">
+                                <?php echo get_theme_mod('lwp-en-home-callout-email')?>
+                            </p>
                         </a>
                     </div>
                     <div class="col-lg-6 col-6 p-0 d-flex justify-content-center">
-                        <a class="d-flex" href="">
+                        <a class="d-flex" href="mailto:<?php echo get_theme_mod('lwp-en-contact-callout-number')?>">
                             <i class="fas fa-phone-alt"></i>
-                            <p class="mb-0 ml-2">+506 2222 2222</p>
+                            <p class="mb-0 ml-2">
+                                <?php echo get_theme_mod('lwp-en-contact-callout-number')?>
+                            </p>
                         </a>
                     </div>
             </div>
