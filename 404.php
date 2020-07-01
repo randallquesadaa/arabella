81d1fdbf79ffbe494dd3ceee2d3ec6802fe92016
luismi97 <luismiguelmolina8@gmail.com>
2020-07-01 00:13:22 -0600

    Mejoras

---
+++ 404.php
@@ -0,0 +1,12 @@
+
+<?php get_header();?>
+
+<div class="container h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col my-5 animate__heartBeat">
+            <img class="img-fluid p-0 w-100" src="<?php echo wp_get_attachment_url(get_theme_mod('lwp-img-callout-img404'))?>">
+        </div>
+    </div>
+</div>
+
+<?php get_footer();?>
